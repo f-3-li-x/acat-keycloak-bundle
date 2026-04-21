8817dfa793351a0abad60f3cf6ad3259942ac659
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2026-04-21 20:28:49 +0200

    service is public now

--- README.md
+++
@@ -1,1 +0,0 @@
-# acat-keycloak-bundle
--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -11,6 +11,6 @@ return static function (ContainerConfigurator $configurator): void {
         ->autoconfigure()
         ->public();
 
-    $services->alias('acat_keycloak.user_provider', KeycloakProvider::class);
+    $services->alias('acat_keycloak.user_provider', KeycloakProvider::class)->public();
 
 };
